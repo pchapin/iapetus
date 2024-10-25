d6fbbe4d10841aa89a61b3e610532faf45720076
Peter Chapin <2094217+pchapin@users.noreply.github.com>
2024-10-24 21:06:53 -0400

    More work on the UI prototype.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,48 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
+
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+import 'react-markdown-editor-lite/lib/index.css';
+
+import Calendar from 'react-calendar';
+import "react-calendar/dist/Calendar.css";
+
 import './App.css';
-import IapetusCalendar from './IapetusCalendar.jsx';
-import SimpleMDE from 'react-simplemde-editor';
-import 'simplemde/src/css/simplemde.css';
 
 function App() {
   const [text, setText] = useState('Hello, World!');
+  const [date, setDate] = useState(new Date());
+
+  // We need a Markdown parser to render the text.
+  const mdParser = new MarkdownIt();
+
+  // When the date changes, update the text to include the new date.
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+    setText(`# ${newDate.toDateString()}\n\n${text}`);
+  };
 
   return (
       <>
         <div className="row justify-content-start">
           <div className="col-9">
-            <h1 style={{ textAlign: 'center' }}>Notebook</h1>
-            <SimpleMDE
+            <h1 style={{textAlign: 'center'}}>Notebook</h1>
+            <MdEditor
                 value={text}
-                onChange={setText}
-                options={{
-                  lineNumbers: false,  // Example option to simplify editor rendering
-                  spellChecker: false, // Disable spellcheck if it's adding to the issue
-                }}
+                style={{height: '500px'}}
+                renderHTML={(text) => mdParser.render(text)}
+                onChange={({text}) => setText(text)}
             />
           </div>
           <div className="col-3">
-            <IapetusCalendar />
+            <Calendar
+                value={date}
+                onChange={handleDateChange}
+                minDate={new Date(2020, 0, 1)}    // Minimum date: January 1, 2023
+                maxDate={new Date(2039, 11, 31)}  // Maximum date: December 31, 2024
+                showNeighboringMonth={false}
+            />
+            <p>Selected date: {date.toDateString()}</p>
           </div>
         </div>
       </>
